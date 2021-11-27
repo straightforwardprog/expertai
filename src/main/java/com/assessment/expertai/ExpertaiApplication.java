052ab6131cd76efa4e5d18cd9bf8325f411fe85d
tremondousit <ibr.bouziane@gmail.com>
2021-11-27 14:55:49 +0100

    first commit

---
+++ src/main/java/com/assessment/expertai/ExpertaiApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.expertai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertaiApplication {
+    //expert AI
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assessment/expertai/ExpertaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.expertai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpertaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
